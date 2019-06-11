d050815bbaa874539b46ee443e62ac917b6ced53
Henry Harris IV <henryharrisiv@Henrys-MacBook-Pro-2.local>
2019-06-11 17:59:19 -0400

    removed unnecessary code

--- App.js
+++ App.js
@@ -7,8 +7,6 @@ import {
 import Card from './src/components/Card';
 import Header from './src/components/Header';
 
-const profileImage = 'https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/23172895_1438203092916138_4641129154353713970_n.png?_nc_cat=1&_nc_oc=AQmjxrC3dmLJWSej8bvlNjEuFhfdUhDacpHX2hNqufib-Sm-MFD38xHe1j3xN5dFqLDzDdyL5JySkzrj2Euw9KTW&_nc_ht=scontent-iad3-1.xx&oh=4252b960c66120475231b538f2c5f237&oe=5D7EC494';
-
 export default class App extends Component {
   render() {
     return (
